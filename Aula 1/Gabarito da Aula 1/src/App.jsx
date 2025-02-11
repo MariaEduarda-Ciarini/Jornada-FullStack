3294bcd8f044a1dc3b5349e403db273b7bb2f09c
MariaEduarda-Ciarini <DudaCiarinii@gmail.com>
2025-02-11 16:27:04 -0300

    Jornada Full Stack - Aula 1
    2:21

---
+++ Aula 1/Gabarito da Aula 1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Main2 from "./Main2";
+
+const App = () => {
+  return (
+  <>
+      <Header/>
+      <Main2/>
+   </>
+  );
+};
+
+export default App
---
+++ Aula 1/Gabarito da Aula 1/src/Main2.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Main2 = () => {
+  return (
+    <div className='main2'>
+        <div className='item-list'>
+    <div className='item-list__header'>
+      <h2>Artistas populares</h2>
+  <a className='item-list__link' href='/'>Mostrar tudo</a>
+    <div className='single-item'>
+        <img className='single-item__image' src='https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4' alt='Imagem do Artista' />
+    <p>Henrique & Juliano</p>
+    <p>Artista</p>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default Main2;
